b16aca3b0961be0739fa007a6faf26062e8244b5
elmakhtar10 <mohamedelmakhtarba@gmail.com>
2026-02-05 16:56:10 +0000

    first commit

---
+++ src/test/java/com/modelsis/backend/ModelsisBackendMohamedElMakhatarBaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modelsis.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelsisBackendMohamedElMakhatarBaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
